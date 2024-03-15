73db527b36530052721600ec9f180a9d6b6de5d9
Konstantinos Peratinos <kotsosgr@gmail.com>
2024-03-15 01:57:13 +0100

    disalowed cors credentials

--- src/main/java/com/konstantinos/zito/security/SecurityConfiguration.java
+++ src/main/java/com/konstantinos/zito/security/SecurityConfiguration.java
@@ -72,7 +72,7 @@ public class SecurityConfiguration {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
+        // config.setAllowCredentials(true);
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
